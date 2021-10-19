4424bee0fe0d7fc64fedbcee0e6d1b2bfda95d08
Krunal2903 <86333958+Krunal2903@users.noreply.github.com>
2021-10-19 20:58:46 +0530

    Create U2_ex_P1.c

---
+++ U2_ex_P1.c
@@ -0,0 +1,25 @@
+/*unit 2 extra program 1*/
+
+#include<stdio.h>
+
+void main()
+{
+    int a,b;
+    
+    printf("\n Enter the first number: ");
+    scanf("%d",&a);
+    
+    printf("\n Enter the second number:");
+    scanf("%d",&b);
+    
+    if(a>b)
+    {
+        printf("\n sum of two number is %d",a+b);
+    }
+    
+    else
+    {    
+        printf("\n multiplication of two number isv%d",a*b);
+    }
+
+}    
